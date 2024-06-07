2ef7834b709409d2494bb7f18fb32a01a498644d
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-06-07 20:54:03 +1000

    Playing around with bootstrap

--- website/js/DualAxisChart.js
+++ website/js/DualAxisChart.js
@@ -1,91 +1,15 @@
-window.onload = startChart;
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function startChart() {
-    let country = getParameterByName('country') || 'Afghanistan';
-    let data_file_path = '../data_processing/processed_data/data_complete.json';
-    d3.json(data_file_path).then(data => {
-        generateRadioButtons(data, country);
-        generateLegend();
-        drawChart(data, country);
-        updateChart(data, country);
-    }).catch(error => {
-        console.error('Error loading JSON data:', error);
-    });
-}
-
-function generateRadioButtons(data, selectedCountry) {
-    var radioContainer = document.getElementById('radioContainer');
-    radioContainer.innerHTML = '';
-    data.forEach((d, i) => {
-        let innerRadioContainer = document.createElement('div');
-        var radio = document.createElement('input');
-
-        radio.addEventListener('change', (event) => {
-            if (event.currentTarget.checked) {
-                updateChart(data, event.currentTarget.value);
-            }
-        });
-
-        radio.type = 'radio';
-        radio.id = 'radio' + i;
-        radio.name = 'country';
-        radio.value = d.name;
-        radio.checked = (d.name === selectedCountry);
-        radio.className = 'radio';
-        var label = document.createElement('label');
-        label.htmlFor = 'radio' + i;
-        label.appendChild(document.createTextNode(d.name));
-
-        innerRadioContainer.appendChild(radio);
-        innerRadioContainer.appendChild(label);
-        radioContainer.appendChild(innerRadioContainer);
-    });
-}
-
-function generateLegend() {
-    var legendContainer = document.getElementById('legendContainer');
-    legendContainer.innerHTML = '';
-
-    const legendData = [
-        { name: 'Fruit Consumption', color: 'steelblue' },
-        { name: 'Cardiovascular Incidences', color: 'red' }
-    ];
-
-    legendData.forEach((d, i) => {
-        let legendItem = document.createElement('div');
-        let colorBox = document.createElement('span');
-        colorBox.style.display = 'inline-block';
-        colorBox.style.width = '10px';
-        colorBox.style.height = '10px';
-        colorBox.style.backgroundColor = d.color;
-        colorBox.style.marginRight = '5px';
-        legendItem.appendChild(colorBox);
-
-        let text = document.createTextNode(d.name);
-        legendItem.appendChild(text);
-
-        legendContainer.appendChild(legendItem);
-    });
-}
-
+// Main function to draw the initial chart
 function drawChart(data, selectedCountry) {
+    // Define SVG dimensions and margins
     const svg = d3.select("svg"),
         margin = { top: 20, right: 80, bottom: 70, left: 60 },
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom;
 
+    // Append a group element to SVG and translate it to accommodate margins
     const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
+    // Append x, y, and y1 axes groups
     g.append("g")
         .attr("class", "axis axis--x")
         .attr("transform", `translate(0,${height})`);
@@ -97,6 +21,7 @@ function drawChart(data, selectedCountry) {
         .attr("class", "axis axis--y1")
         .attr("transform", `translate(${width},0)`);
 
+    // Append x, y, and y1 axis labels
     g.append("text")
         .attr("class", "x-label")
         .attr("fill", "#FFF")
@@ -125,7 +50,7 @@ function drawChart(data, selectedCountry) {
         .attr("text-anchor", "middle")
         .text("Cardiovascular Incidences");
 
-    // Define tooltip once
+    // Define and append tooltip
     const tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("position", "absolute")
@@ -137,38 +62,48 @@ function drawChart(data, selectedCountry) {
         .style("padding", "10px")
         .style("pointer-events", "none");
 
+    // Call updateChart to draw initial data
     updateChart(data, selectedCountry, tooltip);
 }
 
+// Function to update the chart with new data
 function updateChart(data, selectedCountry, tooltip) {
+    // Retrieve SVG, margins, and dimensions
     const svg = d3.select("svg"),
         margin = { top: 20, right: 80, bottom: 70, left: 60 },
         width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
         g = svg.select("g");
 
+    // Define x, y0, and y1 scales
     const x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
         y0 = d3.scaleLinear().rangeRound([height, 0]),
         y1 = d3.scaleLinear().rangeRound([height, 0]);
 
+    // Retrieve data for the selected country
     const countryData = data.find(d => d.name === selectedCountry);
     const fruitConsumption = countryData.fruit_consumption.map(v => ({ ...v, country: countryData.name }));
     const cardiovascularIncidences = countryData.cardiovascular_incidences.map(v => ({ ...v, country: countryData.name }));
 
+    // Calculate start and end years for the selected data
     const startYear = d3.max([d3.min(fruitConsumption, d => d.year), d3.min(cardiovascularIncidences, d => d.year)]);
     const endYear = d3.min([d3.max(fruitConsumption, d => d.year), d3.max(cardiovascularIncidences, d => d.year)]);
 
+    // Filter data for the selected range of years
     const filteredFruitConsumption = fruitConsumption.filter(d => d.year >= startYear && d.year <= endYear);
     const filteredCardiovascularIncidences = cardiovascularIncidences.filter(d => d.year >= startYear && d.year <= endYear);
 
+    // Set domains for x, y0, and y1 scales
     x.domain(filteredFruitConsumption.map(d => d.year));
     y0.domain([0, 500]).nice();
     y1.domain([0, 1.6]).nice();
 
+    // Define color palette and color scale
     let color_pallete = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'];
     let color = d3.scaleQuantize().range(color_pallete);
     color.domain([1, 500]);
 
+    // Update x, y, and y1 axes
     g.select(".axis--x")
         .call(d3.axisBottom(x))
         .selectAll("text")
@@ -186,9 +121,11 @@ function updateChart(data, selectedCountry, tooltip) {
         .transition().duration(1000)
         .call(d3.axisRight(y1));
 
+    // Update or append bars for fruit consumption data
     const bars = g.selectAll(".bar")
         .data(filteredFruitConsumption, d => d.year);
 
+    // Handle enter selection for bars
     bars.enter().append("rect")
         .attr("class", "bar")
         .attr("x", d => x(d.year))
@@ -204,20 +141,25 @@ function updateChart(data, selectedCountry, tooltip) {
         .attr("height", d => height - y0(d.value))
         .attr("fill", d => color(d.value));
 
+    // Handle exit selection for bars
     bars.exit().remove();
 
+    // Add event listeners for bars to show tooltip
     bars.on("mouseover", function(event, d) {
             tooltip.style("visibility", "visible")
-                .html(`Year: ${d.year}<br>Fruit Consumption: ${d.value}`);
+                .html(`Year: ${d.year}<br>Fruit Consumption: ${d.value}`)
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 15) + "px");
         })
         .on("mousemove", function(event) {
-            tooltip.style("top", (event.pageY - 10) + "px")
-                .style("left", (event.pageX + 10) + "px");
+            tooltip.style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 15) + "px");
         })
         .on("mouseout", function() {
             tooltip.style("visibility", "hidden");
         });
 
+    // Update or append line for cardiovascular incidences data
     const line = d3.line()
         .x(d => x(d.year) + x.bandwidth() / 2)
         .y(d => y1(d.value));
@@ -225,312 +167,14 @@ function updateChart(data, selectedCountry, tooltip) {
     const path = g.selectAll(".line")
         .data([filteredCardiovascularIncidences]);
 
+    // Handle enter selection for line
     path.enter().append("path")
         .attr("class", "line")
-        .attr("stroke", "#00805A")
-        .attr("stroke-width", 2)
-        .attr("fill", "none")
         .merge(path)
         .transition()
         .duration(1000)
         .attr("d", line);
 
+    // Handle exit selection for line
     path.exit().remove();
-
-    const lineDots = g.selectAll(".line-dot")
-        .data(filteredCardiovascularIncidences, d => d.year);
-
-    lineDots.enter().append("circle")
-        .attr("class", "line-dot")
-        .attr("cx", d => x(d.year) + x.bandwidth() / 2)
-        .attr("cy", d => y1(d.value))
-        .attr("r", 4)
-        .attr("fill", "red");
-
-    lineDots.transition()
-        .duration(1000)
-        .attr("cx", d => x(d.year) + x.bandwidth() / 2)
-        .attr("cy", d => y1(d.value));
-
-    lineDots.exit().remove();
-
-    lineDots.on("mouseover", function(event, d) {
-            tooltip.style("visibility", "visible")
-                .html(`Year: ${d.year}<br>Cardiovascular Incidences: ${d.value}`);
-        })
-        .on("mousemove", function(event) {
-            tooltip.style("top", (event.pageY - 10) + "px")
-                .style("left", (event.pageX + 10) + "px");
-        })
-        .on("mouseout", function() {
-            tooltip.style("visibility", "hidden");
-        });
-}
-
-function updateChart(data, selectedCountry, tooltip) {
-    const svg = d3.select("svg"),
-        margin = { top: 20, right: 80, bottom: 70, left: 60 },
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.select("g");
-
-    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y0 = d3.scaleLinear().rangeRound([height, 0]),
-        y1 = d3.scaleLinear().rangeRound([height, 0]);
-
-    const countryData = data.find(d => d.name === selectedCountry);
-    const fruitConsumption = countryData.fruit_consumption.map(v => ({ ...v, country: countryData.name }));
-    const cardiovascularIncidences = countryData.cardiovascular_incidences.map(v => ({ ...v, country: countryData.name }));
-
-    const startYear = d3.max([d3.min(fruitConsumption, d => d.year), d3.min(cardiovascularIncidences, d => d.year)]);
-    const endYear = d3.min([d3.max(fruitConsumption, d => d.year), d3.max(cardiovascularIncidences, d => d.year)]);
-
-    const filteredFruitConsumption = fruitConsumption.filter(d => d.year >= startYear && d.year <= endYear);
-    const filteredCardiovascularIncidences = cardiovascularIncidences.filter(d => d.year >= startYear && d.year <= endYear);
-
-    x.domain(filteredFruitConsumption.map(d => d.year));
-    y0.domain([0, 500]).nice();
-    y1.domain([0, 1.6]).nice();
-
-    let color_pallete = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'];
-    let color = d3.scaleQuantize().range(color_pallete);
-    color.domain([1, 500]);
-
-    g.select(".axis--x")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("x", -5)
-        .attr("y", 10)
-        .attr("dy", ".35em")
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "end");
-
-    g.select(".axis--y")
-        .transition().duration(1000)
-        .call(d3.axisLeft(y0));
-
-    g.select(".axis--y1")
-        .transition().duration(1000)
-        .call(d3.axisRight(y1));
-
-    const bars = g.selectAll(".bar")
-        .data(filteredFruitConsumption, d => d.year);
-
-    bars.enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(d.year))
-        .attr("y", height)
-        .attr("width", x.bandwidth())
-        .attr("height", 0)
-        .merge(bars)
-        .transition()
-        .duration(1000)
-        .attr("x", d => x(d.year))
-        .attr("y", d => y0(d.value))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y0(d.value))
-        .attr("fill", d => color(d.value));
-
-    bars.exit().remove();
-
-    bars.on("mouseover", function(event, d) {
-            tooltip.style("visibility", "visible")
-                .html(`Year: ${d.year}<br>Fruit Consumption: ${d.value}`);
-        })
-        .on("mousemove", function(event) {
-            tooltip.style("top", (event.pageY - 10) + "px")
-                .style("left", (event.pageX + 10) + "px");
-        })
-        .on("mouseout", function() {
-            tooltip.style("visibility", "hidden");
-        });
-
-    const line = d3.line()
-        .x(d => x(d.year) + x.bandwidth() / 2)
-        .y(d => y1(d.value));
-
-    const path = g.selectAll(".line")
-        .data([filteredCardiovascularIncidences]);
-
-    path.enter().append("path")
-        .attr("class", "line")
-        .attr("stroke", "#00805A")
-        .attr("stroke-width", 2)
-        .attr("fill", "none")
-        .merge(path)
-        .transition()
-        .duration(1000)
-        .attr("d", line);
-
-    path.exit().remove();
-
-    const lineDots = g.selectAll(".line-dot")
-        .data(filteredCardiovascularIncidences, d => d.year);
-
-    lineDots.enter().append("circle")
-        .attr("class", "line-dot")
-        .attr("cx", d => x(d.year) + x.bandwidth() / 2)
-        .attr("cy", d => y1(d.value))
-        .attr("r", 4)
-        .attr("fill", "red");
-
-    lineDots.transition()
-        .duration(1000)
-        .attr("cx", d => x(d.year) + x.bandwidth() / 2)
-        .attr("cy", d => y1(d.value));
-
-    lineDots.exit().remove();
-
-    lineDots.on("mouseover", function(event, d) {
-            tooltip.style("visibility", "visible")
-                .html(`Year: ${d.year}<br>Cardiovascular Incidences: ${d.value}`);
-        })
-        .on("mousemove", function(event) {
-            tooltip.style("top", (event.pageY - 10) + "px")
-                .style("left", (event.pageX + 10) + "px");
-        })
-        .on("mouseout", function() {
-            tooltip.style("visibility", "hidden");
-        });
-}
-
-function updateChart(data, selectedCountry) {
-    const svg = d3.select("svg"),
-        margin = { top: 20, right: 80, bottom: 70, left: 60 },
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.select("g");
-
-    const x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y0 = d3.scaleLinear().rangeRound([height, 0]),
-        y1 = d3.scaleLinear().rangeRound([height, 0]);
-
-    const countryData = data.find(d => d.name === selectedCountry);
-    const fruitConsumption = countryData.fruit_consumption.map(v => ({ ...v, country: countryData.name }));
-    const cardiovascularIncidences = countryData.cardiovascular_incidences.map(v => ({ ...v, country: countryData.name }));
-
-    const startYear = d3.max([d3.min(fruitConsumption, d => d.year), d3.min(cardiovascularIncidences, d => d.year)]);
-    const endYear = d3.min([d3.max(fruitConsumption, d => d.year), d3.max(cardiovascularIncidences, d => d.year)]);
-
-    const filteredFruitConsumption = fruitConsumption.filter(d => d.year >= startYear && d.year <= endYear);
-    const filteredCardiovascularIncidences = cardiovascularIncidences.filter(d => d.year >= startYear && d.year <= endYear);
-
-    x.domain(filteredFruitConsumption.map(d => d.year));
-    y0.domain([0, 500]).nice();
-    y1.domain([0, 1.6]).nice();
-
-    let color_pallete = ['#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'];
-    let color = d3.scaleQuantize().range(color_pallete);
-    color.domain([1, 500]);
-
-    g.select(".axis--x")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-        .attr("x", -5)
-        .attr("y", 10)
-        .attr("dy", ".35em")
-        .attr("transform", "rotate(-45)")
-        .style("text-anchor", "end");
-
-    g.select(".axis--y")
-        .transition().duration(1000)
-        .call(d3.axisLeft(y0));
-
-    g.select(".axis--y1")
-        .transition().duration(1000)
-        .call(d3.axisRight(y1));
-
-    const bars = g.selectAll(".bar")
-        .data(filteredFruitConsumption, d => d.year);
-
-    const tooltip = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("position", "absolute")
-        .style("visibility", "hidden")
-        .style("background-color", "#2B2B2B")
-        .style("border", "solid")
-        .style("border-width", "1px")
-        .style("border-radius", "5px")
-        .style("padding", "10px")
-        .style("pointer-events", "none");
-
-    bars.enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", d => x(d.year))
-        .attr("y", height)
-        .attr("width", x.bandwidth())
-        .attr("height", 0)
-        .merge(bars)
-        .transition()
-        .duration(1000)
-        .attr("x", d => x(d.year))
-        .attr("y", d => y0(d.value))
-        .attr("width", x.bandwidth())
-        .attr("height", d => height - y0(d.value))
-        .attr("fill", d => color(d.value));
-
-    bars.exit().remove();
-
-    bars.on("mouseover", function(event, d) {
-            tooltip.style("visibility", "visible")
-                .html(`Year: ${d.year}<br>Fruit Consumption: ${d.value}`);
-        })
-        .on("mousemove", function(event) {
-            tooltip.style("top", (event.pageY - 10) + "px")
-                .style("left", (event.pageX + 10) + "px");
-        })
-        .on("mouseout", function() {
-            tooltip.style("visibility", "hidden");
-        });
-
-    const line = d3.line()
-        .x(d => x(d.year) + x.bandwidth() / 2)
-        .y(d => y1(d.value));
-
-    const path = g.selectAll(".line")
-        .data([filteredCardiovascularIncidences]);
-
-    path.enter().append("path")
-        .attr("class", "line")
-        .attr("stroke", "#00805A")
-        .attr("stroke-width", 2)
-        .attr("fill", "none")
-        .merge(path)
-        .transition()
-        .duration(1000)
-        .attr("d", line);
-
-    path.exit().remove();
-
-    const lineDots = g.selectAll(".line-dot")
-        .data(filteredCardiovascularIncidences, d => d.year);
-
-    // Handle enter selection
-    lineDots.enter().append("circle")
-        .attr("class", "line-dot")
-        .attr("cx", d => x(d.year) + x.bandwidth() / 2)
-        .attr("cy", d => y1(d.value))
-        .attr("r", 4)
-        .attr("fill", "green");
-
-    // Handle update selection
-    lineDots.transition()
-        .duration(1000)
-        .attr("cx", d => x(d.year) + x.bandwidth() / 2)
-        .attr("cy", d => y1(d.value));
-
-    // Handle exit selection
-    lineDots.exit().remove();
-
-    lineDots.on("mouseover", function(event, d) {
-            tooltip.style("visibility", "visible")
-                .html(`Year: ${d.year}<br>Cardiovascular Incidences: ${d.value}`);
-        })
-        .on("mousemove", function(event) {
-            tooltip.style("top", (event.pageY - 10) + "px")
-                .style("left", (event.pageX + 10) + "px");
-        })
-        .on("mouseout", function() {
-            tooltip.style("visibility", "hidden");
-        });
 }
